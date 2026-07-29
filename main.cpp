275931a4ce356e1aa33cb323870195344667794e
acostahorn <otreblatsoc@gmail.com>
2026-07-29 13:01:43 +0100

    added Icon to Build directory

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <shobjidl.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    SetCurrentProcessExplicitAppUserModelID(L"Genova.Cirulla.Game");
 
 
     a.setStyleSheet("QPushButton { border: 1px solid #8f8f91; border-radius: 4px; background-color: #f0f0f0; padding: 5px}"
